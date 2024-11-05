e7c5c4712d673716f7fff0b503f6cd3a3fa03e47
ivank <ivank@IVAN>
2024-11-05 20:48:45 +0300

    Добавьте файлы проекта.

---
+++ pokrovskoe-streshnego3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.Write("Введите ФИО: ");
+var name = Console.ReadLine();
+
+Console.Write("Введите дату рождения: ");
+var dateOfBirth = Console.ReadLine();
+
+Console.Write("Введите номер телефона: ");
+var number = Console.ReadLine();
+
+Console.Write("Введите почту: ");
+var email = Console.ReadLine();
+
+DateTime birthDate = DateTime.Parse(dateOfBirth);
+DateTime currentDate = DateTime.Now;
+int age = currentDate.Year - birthDate.Year;
+if (currentDate.Month < birthDate.Month ||
+(currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
+{
+    age--;
+}
+
+
+Console.WriteLine($"Привет {name}\nДата: {dateOfBirth}\nНомер: {number}\nПочта: {email}\nВаш возраст: {age}");
